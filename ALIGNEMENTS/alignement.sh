08d66a354bc5dc7c99e9bededc8e939b4e8dfefd
michel_boissac <101632678+Michelboissac@users.noreply.github.com>
2025-10-14 10:51:52 +0200

    Add files via upload

--- ALIGNEMENTS/alignement.sh
+++ ALIGNEMENTS/alignement.sh
@@ -1,11 +1,15 @@
 DOSSIER_TRAVAIL=$1
 DOSSIER_GENOME_TRAVAIL=$2
-extension=$3
-DOSSIER_READS_TRAVAIL=$4
-NOM_BASE_PROJET=$5
-liste_sra_a_telecharger=$6
-technologie_de_sequencage=$7
-single_or_pair_end=$8
+url_genome_fna=$3
+url_genome_gtf=$4
+
+
+DOSSIER_READS_TRAVAIL=$5
+NOM_BASE_PROJET=$6
+liste_sra_a_telecharger=$7
+technologie_de_sequencage=$8
+single_or_pair_end=$9
+
 
 
 
@@ -16,19 +20,23 @@ cd --
 nom_matrice_alignement="${NOM_BASE_PROJET}_${technologie_de_sequencage}_${single_or_pair_end}_counts.txt"
 DOSSIER_TRAVAIL_projet="${DOSSIER_TRAVAIL}${NOM_BASE_PROJET}/"
 
-#genome fasta
-nom_genome=GCF_003254395.2_Amel_HAv3.1_genomic.fna
-url_genome_fna=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.fna.gz
+#genome fasta et gtf 
 
-#genome annotation gtf
-nom_genome_annotation=GCF_003254395.2_Amel_HAv3.1_genomic.gtf
-url_genome_gtf=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.gtf.gz
+#url_genome_fna=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.fna.gz
+#url_genome_gtf=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.gtf.gz
 
 
-DOSSIER_SAM=${DOSSIER_TRAVAIL_projet}SAM/
-DOSSIER_SAM_STATS=${DOSSIER_TRAVAIL_projet}SAM_STATS/
+# Extraction du nom de fichier sans .gz
+nom_genome=$(basename "$url_genome_fna" .gz)
+nom_genome_annotation=$(basename "$url_genome_gtf" .gz)
 
-DOSSIER_BAM=${DOSSIER_TRAVAIL_projet}BAM/
+# Vérification
+echo "nom_genome = $nom_genome"
+echo "nom_genome_annotation = $nom_genome_annotation"
+
+
+
+DOSSIER_STATS=${DOSSIER_TRAVAIL_projet}STATS/
 DOSSIER_SORTED_BAM_and_INDEX=${DOSSIER_TRAVAIL_projet}SORTED_BAM_AND_INDEX/
 DOSSIER_COUNTS=${DOSSIER_TRAVAIL_projet}COUNTS/
 
@@ -38,9 +46,9 @@ liste_alignements=()
 mkdir "${DOSSIER_TRAVAIL[@]}"
 mkdir "${DOSSIER_TRAVAIL_projet[@]}"
 
-mkdir "${DOSSIER_SAM[@]}"
-mkdir "${DOSSIER_SAM_STATS[@]}"
-mkdir "${DOSSIER_BAM[@]}"
+
+mkdir "${DOSSIER_STATS[@]}"
+
 mkdir "${DOSSIER_SORTED_BAM_and_INDEX[@]}"
 mkdir "${DOSSIER_COUNTS[@]}"
 
@@ -49,19 +57,14 @@ cd $DOSSIER_TRAVAIL_projet
 
 
 
-
-
 #####################################################################
-#telecharge genome illumina
 
+#telecharge genome illumina
 echo $technologie_de_sequencage
 echo ${DOSSIER_GENOME_TRAVAIL[@]}
-
 if [ ${DOSSIER_GENOME_TRAVAIL[@]} == "telecharger" ]
 then
-
     echo "Telechargement du genome fasta, et des annotations gtf"
-
     #
     #indexation :
     if [ $technologie_de_sequencage == "long" ]
@@ -71,19 +74,19 @@ then
         genome=${DOSSIER_GENOME_TRAVAIL}${nom_genome}
         genome_index=${DOSSIER_GENOME_TRAVAIL}${nom_genome}.idx
         genome_annotation_gtf=${DOSSIER_GENOME_TRAVAIL}${nom_genome_annotation}
-
-
         mkdir "${DOSSIER_GENOME_TRAVAIL[@]}"
-        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_gtf}
-        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_fna}
-
+        
+        # Téléchargements parallèles avec wget
+        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_gtf} &
+        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_fna} &
+        wait
+        
+        # Décompression
         gunzip "${DOSSIER_GENOME_TRAVAIL[@]}/"*
-
-        minimap2 -d "${genome_index[@]}" "${genome[@]}"
+        
+        # Indexation avec threads
+        minimap2 -t 28 -d "${genome_index[@]}" "${genome[@]}"
     fi
-
-
-
     if [ $technologie_de_sequencage == "short" ]
     then
         echo "telechargement et indexation du genome pour un alignement des shorts reads avec hisat2"
@@ -93,74 +96,102 @@ then
         genome_annotation_gtf=${DOSSIER_GENOME_TRAVAIL}${nom_genome_annotation}
         splicesites=${DOSSIER_GENOME_TRAVAIL}splicesites.txt
         exons=${DOSSIER_GENOME_TRAVAIL}exons.txt
-
         mkdir "${DOSSIER_GENOME_TRAVAIL[@]}"
-
-        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_gtf}
-        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_fna}
-
+        
+        # Téléchargements parallèles avec wget
+        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_gtf} &
+        wget -P $DOSSIER_GENOME_TRAVAIL ${url_genome_fna} &
+        wait
+        
+        # Décompression
         gunzip "${DOSSIER_GENOME_TRAVAIL[@]}/"*
-
-        hisat2_extract_splice_sites.py "${genome_annotation_gtf[@]}" > "${splicesites[@]}"
-        hisat2_extract_exons.py "${genome_annotation_gtf[@]}" > "${exons[@]}"
-        hisat2-build --ss "${splicesites[@]}" --exon "${exons[@]}" "${genome[@]}" "${genome_index[@]}"
+        
+        # Extraction parallèle des splicesites et exons
+        hisat2_extract_splice_sites.py "${genome_annotation_gtf[@]}" > "${splicesites[@]}" &
+        hisat2_extract_exons.py "${genome_annotation_gtf[@]}" > "${exons[@]}" &
+        wait
+        
+        # Indexation avec threads
+        hisat2-build -p 28 --ss "${splicesites[@]}" --exon "${exons[@]}" "${genome[@]}" "${genome_index[@]}"
     fi
-
-
 else
    echo "genome deja telechargé et indéxé"
-
     genome=${DOSSIER_GENOME_TRAVAIL}${nom_genome}
     genome_index=${DOSSIER_GENOME_TRAVAIL}${nom_genome}.idx
     genome_annotation_gtf=${DOSSIER_GENOME_TRAVAIL}${nom_genome_annotation}
-
     #hisat2 shorts reads illumina
     splicesites=${DOSSIER_GENOME_TRAVAIL}splicesites.txt
     exons=${DOSSIER_GENOME_TRAVAIL}exons.txt
 fi
-
 echo -e "Le genome, annotation gtf, et index, exons et splicesites sont dans le repertoire : ${DOSSIER_GENOME_TRAVAIL[@]} \n"
 ls $DOSSIER_GENOME_TRAVAIL
-
-
 #####################################################################
-#telecharge sra
-
+# Télécharge sra
 if [ ${DOSSIER_READS_TRAVAIL[@]} == "telecharger" ]
 then
     DOSSIER_READS_TRAVAIL=${DOSSIER_TRAVAIL_projet}SRA/
-    mkdir "${DOSSIER_READS_TRAVAIL[@]}"
+    mkdir -p "${DOSSIER_READS_TRAVAIL[@]}"
     echo $liste_sra_a_telecharger
     echo "telechargement des sra"
+    
+    # Téléchargement parallèle des SRA (max 4 en parallèle pour ne pas surcharger)
+    max_parallel=4
+    count=0
     for sra in $liste_sra_a_telecharger;
     do
         echo $sra
-        fasterq-dump $sra -O "${DOSSIER_READS_TRAVAIL[@]}" -p
+        # fasterq-dump avec threads et split-3 pour pair-end
+        fasterq-dump $sra -O "${DOSSIER_READS_TRAVAIL[@]}" -e 8 --split-3 &
+        
+        ((count++))
+        # Attendre tous les max_parallel téléchargements avant de continuer
+        if [ $((count % max_parallel)) -eq 0 ]; then
+            wait
+        fi
     done
+    wait  # Attendre les derniers téléchargements
 fi
 
-echo -e "Les reads sont dans le dossier :  $DOSSIER_READS_TRAVAIL \n"
+echo -e "Les reads sont dans le dossier : $DOSSIER_READS_TRAVAIL \n"
 ls $DOSSIER_READS_TRAVAIL
 
+# Détection automatique de l'extension FASTQ
+echo "Détection de l'extension des fichiers..."
+if ls "${DOSSIER_READS_TRAVAIL}"*.fastq 1> /dev/null 2>&1; then
+    extension=".fastq"
+    echo "Extension détectée : .fastq"
+elif ls "${DOSSIER_READS_TRAVAIL}"*.fq 1> /dev/null 2>&1; then
+    extension=".fq"
+    echo "Extension détectée : .fq"
+elif ls "${DOSSIER_READS_TRAVAIL}"*.fastq.gz 1> /dev/null 2>&1; then
+    extension=".fastq.gz"
+    echo "Extension détectée : .fastq.gz"
+elif ls "${DOSSIER_READS_TRAVAIL}"*.fq.gz 1> /dev/null 2>&1; then
+    extension=".fq.gz"
+    echo "Extension détectée : .fq.gz"
+else
+    echo "ERREUR : Aucun fichier FASTQ trouvé dans ${DOSSIER_READS_TRAVAIL}"
+    exit 1
+fi
 
-#Recupere la liste des sra.fastq dans le dossier SRA  SINGLE READS
+# Récupère la liste des reads dans le dossier - SINGLE READS
 if [ $single_or_pair_end == "single" ]
 then
-readarray -t liste_de_reads < <(
-    ls "${DOSSIER_READS_TRAVAIL[@]}"*${extension} | sed "s|${DOSSIER_READS_TRAVAIL[@]}||" | sed "s|${extension}||" | sort -u
-)
-echo "${liste_de_reads[@]}"
+    readarray -t liste_de_reads < <(
+        ls "${DOSSIER_READS_TRAVAIL[@]}"*${extension} | sed "s|${DOSSIER_READS_TRAVAIL[@]}||" | sed "s|${extension}||" | sort -u
+    )
+    echo "Reads single-end détectés : ${liste_de_reads[@]}"
 fi
 
-
+# PAIR-END READS
 if [ $single_or_pair_end == "pair" ]
 then
-readarray -t liste_de_reads < <(
-  ls "${DOSSIER_READS_TRAVAIL[@]}"*${extension}|sed "s|${DOSSIER_READS_TRAVAIL[@]}||"|sed "s|[1-2]${extension}||"|sort -u
-)
-echo "${liste_de_reads[@]}"
+    readarray -t liste_de_reads < <(
+        ls "${DOSSIER_READS_TRAVAIL[@]}"*${extension} | sed "s|${DOSSIER_READS_TRAVAIL[@]}||" | sed "s|[_]?[12]${extension}||" | sort -u
+    )
+    echo "Reads pair-end détectés : ${liste_de_reads[@]}"
 fi
-
+#####################################################################
 
 #ALIGNEMENTS
 ################################################################################################
@@ -172,16 +203,22 @@ for nom_reads in "${liste_de_reads[@]}"; do
     reads="${nom_reads}${extension}"
     alignement=${nom_genome}.VS.${nom_reads}
     cd "${DOSSIER_TRAVAIL_projet[@]}"
-    hisat2 -x "${DOSSIER_GENOME_TRAVAIL[@]}${nom_genome}.idx" --dta --rna-strandness R -U "${DOSSIER_READS_TRAVAIL[@]}/${reads}" -S SAM/${alignement}.sam
-    samtools flagstats "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_SAM_STATS[@]}${alignement}.sam.stats.txt"
-    samtools view -Sb "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_BAM[@]}${alignement}.bam"
-    samtools sort "${DOSSIER_BAM[@]}${alignement}.bam" -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
-    samtools index "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
+    
+    # Pipeline optimisé : hisat2 -> samtools view -> samtools sort en un seul flux
+    hisat2 -p 28 -x "${DOSSIER_GENOME_TRAVAIL[@]}${nom_genome}.idx" --dta --rna-strandness R -U "${DOSSIER_READS_TRAVAIL[@]}/${reads}" | \
+    samtools view -@ 8 -Sb - | \
+    samtools sort -@ 8 -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam" -
+    
+    # Index en parallèle
+    samtools index -@ 4 "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
+    
+    # Stats si nécessaire (optionnel)
+    samtools flagstat -@ 4 "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam" > "${DOSSIER_STATS[@]}${alignement}.stats.txt"
+    
     liste_alignements+=("${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam")
 done
 fi
 ################################################################################################
-
 if [[ "$technologie_de_sequencage" == "short" && "$single_or_pair_end" == "pair" ]]
 then
 echo "alignement short reads pair end"
@@ -192,19 +229,21 @@ for nom_reads in "${liste_de_reads[@]}"; do
     alignement=${nom_genome}.VS.${nom_reads}
     cd "${DOSSIER_TRAVAIL_projet[@]}"
     echo "${DOSSIER_GENOME_TRAVAIL[@]}${nom_genome}.idx"
-    hisat2 -x "${DOSSIER_GENOME_TRAVAIL[@]}${nom_genome}.idx" --dta --rna-strandness RF -1 "${DOSSIER_READS_TRAVAIL[@]}/${reads1[@]}" -2 "${DOSSIER_READS_TRAVAIL[@]}/${reads2[@]}" -S SAM/${alignement}.sam
-    samtools flagstats "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_SAM_STATS[@]}${alignement}.sam.stats.txt"
-    samtools view -Sb "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_BAM[@]}${alignement}.bam"
-    samtools sort -n "${DOSSIER_BAM[@]}${alignement}.bam" -o "${DOSSIER_BAM[@]}${alignement}.name_sorted.bam"  # tri par nom pour fixmate
-    samtools fixmate -m "${DOSSIER_BAM[@]}${alignement}.name_sorted.bam" "${DOSSIER_BAM[@]}${alignement}.name_sorted.fixmate.bam"
-    samtools sort "${DOSSIER_BAM[@]}${alignement}.name_sorted.fixmate.bam" -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.name_sorted.fixmate.sorted.bam"
-    samtools index "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.name_sorted.fixmate.sorted.bam"
-    liste_alignements+=("${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.name_sorted.fixmate.sorted.bam")
+    
+    # Pipeline optimisé : hisat2 -> samtools view -> samtools sort (par coordonnées directement)
+    hisat2 -p 28 -x "${DOSSIER_GENOME_TRAVAIL[@]}${nom_genome}.idx" --dta --rna-strandness RF \
+    -1 "${DOSSIER_READS_TRAVAIL[@]}/${reads1[@]}" -2 "${DOSSIER_READS_TRAVAIL[@]}/${reads2[@]}" | \
+    samtools view -@ 8 -Sb - | \
+    samtools sort -@ 8 -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam" -
+    
+    # Index en parallèle
+    samtools index -@ 4 "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
+    
+    liste_alignements+=("${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam")
 done
 fi
-
 ################################################################################################
-
+echo "$single_or_pair_end"
 if [[ "$technologie_de_sequencage" == "long" && "$single_or_pair_end" == "single" ]]
 then
 echo "alignement long reads single end"
@@ -212,11 +251,18 @@ for nom_reads in "${liste_de_reads[@]}";
 do
     reads="${DOSSIER_READS_TRAVAIL[@]}${nom_reads}${extension}"
     alignement=${nom_genome}.VS.${nom_reads}
-    minimap2 -ax splice "${genome[@]}" "${reads[@]}" > "${DOSSIER_SAM[@]}${alignement}.sam"
-    samtools flagstats "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_SAM_STATS[@]}${alignement}.sam.stats.txt"
-    samtools view -Sb "${DOSSIER_SAM[@]}${alignement}.sam" > "${DOSSIER_BAM[@]}${alignement}.bam"
-    samtools sort "${DOSSIER_BAM[@]}${alignement}.bam" -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
-    samtools index "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
+    
+    # Pipeline optimisé : minimap2 -> samtools view -> samtools sort en un seul flux
+    minimap2 -ax splice -t 28 "${genome[@]}" "${reads[@]}" | \
+    samtools view -@ 8 -Sb - | \
+    samtools sort -@ 8 -o "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam" -
+    
+    # Index en parallèle
+    samtools index -@ 4 "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam"
+    
+    # Stats si nécessaire (optionnel, peut être commenté pour gagner du temps)
+    samtools flagstat -@ 4 "${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam" > "${DOSSIER_STATS[@]}${alignement}.stats.txt"
+    
     liste_alignements+=("${DOSSIER_SORTED_BAM_and_INDEX[@]}${alignement}.sorted.bam")
 done
 fi
--- ALIGNEMENTS/alignements.py
+++ ALIGNEMENTS/alignements.py
@@ -1,24 +1,142 @@
 import streamlit as st
 import subprocess
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+def convert_windows_to_wsl_path(windows_path):
+    """Convertit un chemin Windows (C:\\path) en chemin WSL (/mnt/c/path)"""
+    if not windows_path:
+        return ""
+    
+    # Remplace les backslashes par des slashes
+    path = windows_path.replace("\\", "/")
+    
+    # Détecte si c'est un chemin Windows avec lettre de lecteur
+    if len(path) >= 2 and path[1] == ":":
+        drive_letter = path[0].lower()
+        path_without_drive = path[2:]
+        wsl_path = f"/mnt/{drive_letter}{path_without_drive}"
+    else:
+        wsl_path = path
+    
+    # Assure qu'il se termine par /
+    if not wsl_path.endswith("/"):
+        wsl_path += "/"
+    
+    return wsl_path
+
+def select_folder():
+    """Ouvre une fenêtre de sélection de dossier"""
+    root = tk.Tk()
+    root.withdraw()  # Cache la fenêtre principale
+    root.wm_attributes('-topmost', 1)  # Met la fenêtre au premier plan
+    folder_path = filedialog.askdirectory(master=root)
+    root.destroy()
+    return folder_path
 
 st.title("Setup & Analyse")
 
 # Champs pour les paramètres de l'analyse
 st.subheader("Run Analysis")
-param1 = st.text_input("dossier travail", placeholder="/mnt/c/etc/dossier_travail/  remplacer les debuts de chemin : C:/ par /mnt/c/")
-param2 = st.text_input("dossier genome (ou telecharger)", placeholder="telecharger OU /mnt/c/etc/dossier_travail/genome_telechargé_et_indexé/")
-param3 = st.text_input("extension", placeholder=".fastq")
-param4 = st.text_input("dossier reads (ou telecharger)", placeholder="telecharger OU /mnt/c/etc/dossier_travail/reads/")
-param5 = st.text_input("nom base projet", placeholder="PRJ_xxxxxxx_Ovaries    > créer un sous dossier : /mnt/c/etc/dossier_travail/PRJ_xxxxxxx_Ovaries/")
-param6 = st.text_input("liste SRA", placeholder="laisser \"\" si on telecharge les SRA, sinon :\"SRRXXXXXX1 SRRXXXXXX2\" metre \"\" autour")
-param7 = st.text_input("technologie de séquençage short long", placeholder="short ou long")
-param8 = st.text_input("single or pair end ?", placeholder="single ou pair")
+
+# Initialiser les valeurs dans session_state si elles n'existent pas
+if 'param1_value' not in st.session_state:
+    st.session_state.param1_value = ""
+if 'param2_value' not in st.session_state:
+    st.session_state.param2_value = ""
+if 'param5_value' not in st.session_state:
+    st.session_state.param5_value = ""
+
+# Param1 - Dossier de travail
+col1, col2 = st.columns([4, 1])
+with col1:
+    param1 = st.text_input(
+        "Dossier travail", 
+        value=st.session_state.param1_value,
+        placeholder="/mnt/c/etc/dossier_travail/",
+        key="param1_input",
+        help="Remplacer les débuts de chemin : C:/ par /mnt/c/"
+    )
+with col2:
+    st.write("")  # Espace pour aligner avec le text_input
+    st.write("")
+    if st.button("📁", key="btn1"):
+        folder = select_folder()
+        if folder:
+            converted_path = convert_windows_to_wsl_path(folder)
+            st.session_state.param1_value = converted_path
+            st.rerun()
+
+# Param2 - Dossier genome
+col3, col4 = st.columns([4, 1])
+with col3:
+    param2 = st.text_input(
+        "Dossier genome (ou telecharger)", 
+        value=st.session_state.param2_value,
+        placeholder="telecharger OU /mnt/c/etc/dossier_travail/genome_telechargé_et_indexé/",
+        key="param2_input"
+    )
+with col4:
+    st.write("")
+    st.write("")
+    if st.button("📁", key="btn2"):
+        folder = select_folder()
+        if folder:
+            converted_path = convert_windows_to_wsl_path(folder)
+            st.session_state.param2_value = converted_path
+            st.rerun()
+
+# Param3 & 4 - URLs (pas de sélecteur de dossier)
+param3 = st.text_input(
+    "URL genome fasta", 
+    value="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.fna.gz"
+)
+param4 = st.text_input(
+    "URL genome gtf", 
+    value="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.gtf.gz"
+)
 
 
+# Param5 - Dossier reads
+col5, col6 = st.columns([4, 1])
+with col5:
+    param5 = st.text_input(
+        "Dossier reads (ou telecharger)", 
+        value=st.session_state.param5_value,
+        placeholder="telecharger OU /mnt/c/etc/dossier_travail/reads/",
+        key="param5_input"
+    )
+with col6:
+    st.write("")
+    st.write("")
+    if st.button("📁", key="btn6"):
+        folder = select_folder()
+        if folder:
+            converted_path = convert_windows_to_wsl_path(folder)
+            st.session_state.param5_value = converted_path
+            st.rerun()
 
+# Paramètres restants
+param6 = st.text_input(
+    "Nom base projet", 
+    placeholder="PRJ_xxxxxxx_Ovaries    > créer un sous dossier : /mnt/c/etc/dossier_travail/PRJ_xxxxxxx_Ovaries/"
+)
+param7 = st.text_input(
+    "Liste SRA", 
+    placeholder='laisser "" si on telecharge les SRA, sinon :"SRRXXXXXX1 SRRXXXXXX2" mettre "" autour'
+)
+param8 = st.text_input(
+    "Technologie de séquençage short long", 
+    placeholder="short ou long"
+)
+param9 = st.text_input(
+    "Single or pair end ?", 
+    placeholder="single ou pair"
+)
 
 if st.button("Launch Analysis"):
-    cmd = f"bash alignement.sh {param1} {param2} {param3} {param4} {param5} {param6} {param7} {param8}"
+    cmd = f"bash alignement.sh {param1} {param2} {param3} {param4} {param5} {param6} {param7} {param8} {param9}"
     try:
         result = subprocess.run(
             cmd,
@@ -34,13 +152,8 @@ if st.button("Launch Analysis"):
     except Exception as e:
         st.error(f"Exception: {e}")
 
-
-
-import webbrowser
-
+# import webbrowser
 # URL locale de Streamlit
-url = "http://localhost:8501"
-
+# url = "http://localhost:8501"
 # Ouvre le navigateur par défaut
-
-#webbrowser.open(url)
+# webbrowser.open(url)
